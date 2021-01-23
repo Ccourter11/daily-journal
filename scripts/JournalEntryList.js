8136a7068071a6a3fb0df7f0d5fabc638a07fb2e
Ccourter11 <courtercalvin@yahoo.com>
2021-01-22 20:37:34 -0600

    update

--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -4,21 +4,24 @@
  *    there are items in the collection exposed by the
  *    data provider component
  */
-import { useJournalEntries } from "./JournalDataProvider.js"
-import { JournalEntryComponent } from "./JournalEntry.js"
+import { useJournalEntries } from "./JournalDataProvider.js";
+import { JournalEntryComponent } from "./JournalEntry.js";
 
 // DOM reference to where all entries will be rendered
-const entryLog = document.querySelector("#entryLog")
+const entryLog = document.querySelector("#entryLog");
 
 export const EntryListComponent = () => {
-    // Use the journal entry data from the data provider component
-    const entries = useJournalEntries()
+  // Use the journal entry data from the data provider component
+  const entries = useJournalEntries();
 
-    for (const entry of entries) {
-        /*
+  for (const entry of entries) {
+    /*
             Invoke the component that returns an
             HTML representation of a single entry
         */
-        entryLog.innerHTML += ???
-    }
-}
+    entryLog.innerHTML += `<section id="entry--${entry.id}" class="journalEntry">
+        Display the entry's full text, and the date
+        it was entered here.
+    </section>`;
+  }
+};
