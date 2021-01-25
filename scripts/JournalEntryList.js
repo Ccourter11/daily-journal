afe6dc54dac49d79c9a19a7ac815a28697a27402
Ccourter11 <courtercalvin@yahoo.com>
2021-01-25 07:30:21 -0600

    rendering daily journal

--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -8,13 +8,16 @@ import { useJournalEntries } from "./JournalDataProvider.js";
 import { JournalEntryComponent } from "./JournalEntry.js";
 
 // DOM reference to where all entries will be rendered
-const entryLog = document.querySelector("#entryLog");
+const entryLog = document.querySelector(".entryLog");
 
 export const EntryListComponent = () => {
   // Use the journal entry data from the data provider component
-  const entries = useJournalEntries(journal);
+  const entries = useJournalEntries();
+
+  let journalsHTMLRepresentations = "";
 
   for (const entry of entries) {
+    journalsHTMLRepresentations += JournalEntryComponent(entry);
     /*
             Invoke the component that returns an
             HTML representation of a single entry
